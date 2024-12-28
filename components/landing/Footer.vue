7e4829ab60678a40e24852285b24f05810b01b3f
nik879 <78380745+nik879@users.noreply.github.com>
2024-12-28 22:49:26 +0100

    feat: auto scroll, input check, spacing

--- components/landing/Footer.vue
+++ components/landing/Footer.vue
@@ -1,21 +1,23 @@
 <template>
-  <footer class="my-20">
-    <p class="text-center text-sm text-slate-500">
-      Copyright © {{ new Date().getFullYear() }} HoliSec GmbH.
-    </p>
-    <!--
+  <footer>
+    <div class="mt-32 mb-10">
+      <p class="text-center text-sm text-slate-500">
+        Copyright © {{ new Date().getFullYear() }} HoliSec GmbH.
+      </p>
+      <!--
       Can we ask you a favor 🙏
       Please keep this backlink on your website if possible.
     -->
-    <p class="text-center text-xs text-slate-500 mt-1">
-      Erstellt mit
-      <a href="https://web3templates.com" target="_blank" rel="noopener" class="hover:underline">
-        web3templates
-      </a>
-      und
-      <a href="web-check.xyz" target="_blank" rel="noopener" class="hover:underline">
-        Webcheck
-      </a>
-    </p>
+      <p class="text-center text-xs text-slate-500 mt-1">
+        Erstellt mit
+        <a href="https://web3templates.com" target="_blank" rel="noopener" class="hover:underline">
+          web3templates
+        </a>
+        und
+        <a href="web-check.xyz" target="_blank" rel="noopener" class="hover:underline">
+          Webcheck 🚀
+        </a>
+      </p>
+    </div>
   </footer>
 </template>
--- components/landing/Hero.vue
+++ components/landing/Hero.vue
@@ -1,5 +1,5 @@
 <template>
-  <main class="grid lg:grid-cols-2 place-items-center pb-64 md:pt-8">
+  <main class="grid lg:grid-cols-2 place-items-center md:pt-8">
     <div class="p-24 pr-0 md:order-1 hidden md:block">
       <img src="~/assets/img/66223e8f2ba9f44ca225e900_faq.svg" alt="WebRay" loading="eager" format="avif" width="512"
         height="512" />
@@ -14,7 +14,7 @@
         <div class="flex flex-row gap-3">
           <div class="flex-1">
             <UInput size="xl" icon="i-heroicons-magnifying-glass-20-solid" color="primary" variant="outline"
-              placeholder="z.B. google.at" v-model="domainInput" :loading="loading" />
+              placeholder="z.B. google.at" v-model="domainInput" :loading="loading" @keydown.enter="sendDomain" />
           </div>
           <UButton :disabled="!isValidDomain || loading" @click="sendDomain"> {{ loading ? "Lädt..." : "Suchen" }}
           </UButton>
@@ -23,22 +23,23 @@
 
       <!-- Anzeige des Verbindungsstatus -->
       <div class="mt-10 text-xl">
-        <p>Status: {{ isConnected ? 'verbunden' : 'getrennt' }}</p>
         <p v-if="errorMessage" class="text-red-500">Fehler: {{ errorMessage }}</p>
+        <h1 v-if="loading" class="mt-5 text-bold">Der Scan kann bis zu 5min in Anspruch nehmen... ⏲️</h1>
       </div>
     </div>
   </main>
-  <div v-if="parsedData" class="mt-17 md:mt-0 mb-72">
-    <h1 class="text-4xl lg:text-5xl font-bold lg:tracking-tight">
+  <div v-if="parsedData" ref="resultContainer" class="mt-72">
+    <h1 class="text-4xl lg:text-5xl font-bold lg:tracking-tight text-primaryc">
       Sicherheitsüberprüfung der Website
     </h1>
-    <p class="text-lg mt-5 text-slate-600">
+    <p class="text-xl mt-5">
       Basierend auf den neuesten Sicherheits-Scans wurden die folgenden Ergebnisse ermittelt:
     </p>
 
     <!-- Dynamische Anzeige des Sicherheits-Scores und der Status-Zusammenfassung -->
     <div v-if="parsedData" class="mt-9">
-      <h2 class="text-2xl font-semibold">Sicherheits-Score: {{ parsedData.sicherheits_score }}</h2>
+      <h2 class="text-2xl font-semibold">Sicherheits-Score: <span class="text-3xl text-secondaryc">{{
+        parsedData.sicherheits_score }}</span></h2>
       <p class="text-slate-601 mt-2">{{ parsedData.status_zusammenfassung }}</p>
     </div>
 
@@ -46,9 +47,9 @@
     <div class="grid sm:grid-cols-2 md:grid-cols-1 mt-8 gap-8"
       v-if="parsedData?.befunde && parsedData.befunde.length > 0">
       <div v-for="item in parsedData.befunde" :key="item.id"
-        class="flex gap-5 items-start p-4 border border-gray-200 rounded-lg shadow-sm">
+        class="flex gap-5 items-start p-4 border border-primaryc rounded-lg shadow-sm">
         <!-- Severity Icon -->
-        <div class="mt-2 bg-black rounded-full p-2 w-8 h-8 shrink-0">
+        <div class="mt-2 bg-primaryc rounded-full p-2 w-8 h-8 shrink-0">
           <Icon class="text-white" :name="getSeverityIcon(item.schwere)" />
         </div>
 
@@ -65,19 +66,30 @@
       </div>
     </div>
   </div>
+
+
+  <!-- Button Back to Top -->
+  <div v-if="showBackToTop" class="fixed bottom-10 right-10">
+    <UButton icon="i-heroicons-chevron-double-up" size="sm" color="primary" variant="solid" label="Nach oben"
+      :trailing="false" @click="scrollToTop" />
+  </div>
+
 </template>
 
 <script setup lang="ts">
 import { createWebSocket } from "~/components/socket";
 import { ref } from "vue";
+import { nextTick } from "vue";
 
 const isConnected = ref(false); // Status der Verbindung
 const lastMessage = ref(""); // Zuletzt empfangene Nachricht
 const domainInput = ref(""); // Eingabefeld für die Domain
+const resultContainer = ref(null); // Referenz auf den Container
 const isValidDomain = ref(true); // Überprüfung der Domain
 const parsedData = ref<any>(null); // Verarbeitete JSON-Daten
 const loading = ref(false); // Ladezustand
 const errorMessage = ref(""); // Fehlermeldung
+const showBackToTop = ref(false);
 let socket: WebSocket;
 
 // Helper function to determine the severity icon based on severity level
@@ -90,12 +102,26 @@ const getSeverityIcon = (severity) => {
   return 'bx:bxs-help-circle'; // Default icon if severity is undefined
 };
 
+// Event-Handler für das Scrollen
+const handleScroll = () => {
+  const scrollPosition = window.scrollY; // Aktuelle Scroll-Position
+  console.log(scrollPosition)
+  const halfPage = document.documentElement.scrollHeight / 3; // Hälfte der gesamten Seite
+  showBackToTop.value = scrollPosition > halfPage; // Button nur anzeigen, wenn die Hälfte erreicht ist
+};
+
+// Scroll nach oben Funktion
+const scrollToTop = () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+};
+
 
 // RegEx für die Domain-Validierung
 const domainRegex = /^(?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?\.)+(?:[a-z0-9]{2,})$/i;
 
 
 onMounted(() => {
+  window.addEventListener("scroll", handleScroll); // Listener fürs Scrollen aktivieren
   console.log("Hero.vue: Verbindung wird hergestellt...");
   socket = createWebSocket();
 
@@ -106,15 +132,26 @@ onMounted(() => {
   });
 
   // Nachricht empfangen
-  socket.addEventListener("message", (event) => {
+  socket.addEventListener("message", async (event) => {
     console.log("[WebSocket] Nachricht empfangen:", event.data);
     loading.value = false;
+    domainInput.value = "";
+
     errorMessage.value = ""; // Fehler zurücksetzen
     lastMessage.value = event.data;
+
 
     // Versuche, die empfangene Nachricht als JSON zu parsen
     try {
       parsedData.value = JSON.parse(event.data);
+
+      // Warte, bis das DOM aktualisiert wurde
+      await nextTick();
+      // Scroll zu dem Container mit den Ergebnissen
+      if (resultContainer.value) {
+        resultContainer.value.scrollIntoView({ behavior: "smooth", block: "center" });
+      }
+
     } catch (error) {
       errorMessage.value = "Fehler beim Verarbeiten der empfangenen Daten.";
     }
@@ -143,6 +180,7 @@ onMounted(() => {
   });
 });
 
+
 // Überprüfe die Domain mit dem RegEx
 const validateDomain = (domain: string): boolean => {
   return domainRegex.test(domain);
@@ -168,6 +206,7 @@ watch(domainInput, (newDomain) => {
 
 onUnmounted(() => {
   console.log("Hero.vue: Schließe WebSocket...");
+  window.removeEventListener("scroll", handleScroll);
   socket.close();
 });
 </script>
